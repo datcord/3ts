8baefe42f5599bcf5a0edfba84d6dca9a2f407a1
datcord <theodorezisimopoulos@gmail.com>
2025-01-03 18:55:35 +0200

    started adding functionality

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,56 @@
-import { Text, View } from "react-native";
-
+import { Button, StyleSheet, View } from 'react-native';
+function handle() {
+  
+}
+function Square () {
+  return <View style={styles.square}>
+          <Button title='X' color={'black'} onPress={handle}/>
+      </View>;
+}
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to sds this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.row}>
+      <Square></Square>
+      <Square></Square>
+      <Square></Square>
+      </View>
+      <View style={styles.row}>
+      <Square></Square>
+      <Square></Square>
+      <Square></Square>
+      </View>
+      <View style={styles.row}>
+      <Square></Square>
+      <Square></Square>
+      <Square></Square>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  square:{
+    backgroundColor: 'black',
+    margin: 5,
+    fontSize: 24,
+    fontWeight: 'bold',
+    height: 80,
+    width: 80,
+    justifyContent: 'center',
+  },
+});
