d6d128dccea902bf879695ff7dfdb1ee7d12b771
datcord <theodorezisimopoulos@gmail.com>
2025-01-08 13:03:24 +0200

    added basic functionality...more todo

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const startState = Array(9).fill(null);
 
 const Game = () => {
   const [squares, setSquares] = useState(startState);
   const [turn, setTurn] = useState(true); //true is X
-  const [winner, setWinner] = useState('');
-  
+  const [winner, setWinner] = useState("");
+
   const calcWinner = () => {
     const lines = [
       [0, 1, 2],
@@ -17,75 +18,92 @@ const Game = () => {
       [1, 4, 7],
       [2, 5, 8],
       [0, 4, 8],
-      [2, 4, 6]
+      [2, 4, 6],
     ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
         setWinner(squares[a]);
+        console.log(squares[a]);
         return;
       }
     }
-    if (squares.every(square => square)) {
-      setWinner('draw');
+    if (squares.every((square) => square)) {
+      setWinner("draw");
+      console.log("draw");
     }
-  }
+  };
 
-  function Square() {
-    return <View style={styles.square}>
-            <Button title='X' color={'black'} onPress={()=>{
-            }}/>
-        </View>;
-  }
+  const board = (gameState: any[]) => {
+    const gameJsx = [];
+    for (let i = 0; i < 9; i += 3) {
+      const rowJsx = [];
+      for (let j = 0; j < 3; j++) {
+        rowJsx.push(
+          <TouchableOpacity
+            style={styles.square}
+            key={`square_${i + j}`}
+            onPress={() => {
+              handlePress(i + j);
+            }}
+          >
+            <Text style={styles.text}>{gameState[i + j]}</Text>
+          </TouchableOpacity>
+        );
+      }
+      gameJsx.push(
+        <View style={styles.row} key={`row${i}`}>
+          {rowJsx}
+        </View>
+      );
+    }
+    return gameJsx;
+  };
 
   const handlePress = (index: number) => {
-    if (!squares[index] && (winner == '')) {
+    if (!squares[index] && winner == "") {
       const newTable = [...squares];
-      newTable[index] = turn ? 'X' : 'O';
+      newTable[index] = turn ? "X" : "O";
       setSquares(newTable);
       setTurn(!turn);
+      calcWinner();
     }
   };
   const reset = () => {
     setSquares(startState);
     setTurn(true);
-    setWinner('');
+    setWinner("");
   };
-  const firstRow = squares.map((num)=>
-    <View style={styles.square}>
-      <Button title={squares[num]} color={'black'} onPress={()=>{handlePress}}/>
-    </View>
-  );
-  return (
-    <View style = {styles.container}>
-      
-    </View>
-  );
-}
+
+  return <SafeAreaView style={styles.container}>{board(squares)}</SafeAreaView>;
+};
 
 const styles = StyleSheet.create({
   row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
   },
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: '#fff',
+    color: "#fff",
   },
-  square:{
-    backgroundColor: 'black',
+  square: {
+    backgroundColor: "black",
     margin: 5,
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     height: 80,
     width: 80,
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 export default Game;
